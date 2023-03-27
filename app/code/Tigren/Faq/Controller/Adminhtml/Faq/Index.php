5e3d6543fc574a223fa2a1b476f3892b65a080b0
The Anh <trinhtheanh789@Gmail.com>
2023-03-27 16:17:16 +0700

    Add copyright

--- app/code/Tigren/Faq/Controller/Adminhtml/Faq/Index.php
+++ app/code/Tigren/Faq/Controller/Adminhtml/Faq/Index.php
@@ -1,7 +1,8 @@
 <?php
 /*
- * @author Trinhtheanh789
- * Copyright (c) 2023.  Trinhtheanh789. All rights reserved.
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright  Copyright (c)  2023.  Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
  */
 
 namespace Tigren\Faq\Controller\Adminhtml\Faq;
